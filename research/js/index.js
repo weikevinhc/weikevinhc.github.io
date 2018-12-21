87077a688faabc28e2ecf05252fa7ee56e424bd0
weikevinhc <kwei0526@gmail.com>
2018-12-21 01:01:26 -0800

    change test folder to research folder as it is working fine

--- research/js/index.js
+++ research/js/index.js
@@ -1,6 +1,3 @@
-var work = document.getElementById("portfolioItemContainer");
-var workRequest = new XMLHttpRequest();
-
 var link = document.getElementById("socialLinkContent");
 var linkRequest = new XMLHttpRequest();
 
@@ -19,6 +16,10 @@ linkRequest.onreadystatechange = function () {
 };
 linkRequest.send(null);
 
+
+var work = document.getElementById("portfolioItemContainer");
+var workRequest = new XMLHttpRequest();
+
 workRequest.open("GET", "json/items.json", true);
 workRequest.onreadystatechange = function () {
     if (workRequest.readyState === 4) {
@@ -33,3 +34,21 @@ workRequest.onreadystatechange = function () {
     }
 };
 workRequest.send(null);
+
+var work2 = document.getElementById("portfolioItemContainer2");
+var work2Request = new XMLHttpRequest();
+
+work2Request.open("GET", "json/items2.json", true);
+work2Request.onreadystatechange = function () {
+    if (work2Request.readyState === 4) {
+        if (work2Request.status === 200 || work2Request.status == 0) {
+            var html = "";
+            JSON.parse(work2Request.responseText).forEach(function (work2) {
+                console.log(work2.name);
+                html += "<div class=\"col s12 m6 l6\"><div class=\"card hoverable\"><div class=\"card-image waves-effect waves-block waves-light\"><img class=\"activator\" src=" + work2.image + "></div><div class=\"card-content\"><span class=\"card-title activator grey-text text-darken-4\">" + work2.name + "<i class=\"material-icons right\">keyboard_arrow_up</i></span></div><div class=\"card-reveal\"><span class=\"card-title grey-text text-darken-4\">" + work2.name + "<i class=\"material-icons right\">close</i></span><p>" + work2.description + "</p></div></div></div>";
+            });
+            work2.innerHTML = html;
+        }
+    }
+};
+work2Request.send(null);
